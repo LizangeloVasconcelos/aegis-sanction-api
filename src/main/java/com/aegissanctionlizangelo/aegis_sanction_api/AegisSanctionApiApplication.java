da6905432c7fdaf645ab3fb99d74171631738fb5
Lizangelo Vasconcelos <vasctools@gmail.com>
2026-04-22 10:28:44 -0300

    chore: initial spring boot setup

---
+++ src/main/java/com/aegissanctionlizangelo/aegis_sanction_api/AegisSanctionApiApplication.java
@@ -0,0 +1,13 @@
+package com.aegissanctionlizangelo.aegis_sanction_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisSanctionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisSanctionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aegissanctionlizangelo/aegis_sanction_api/AegisSanctionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegissanctionlizangelo.aegis_sanction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisSanctionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
